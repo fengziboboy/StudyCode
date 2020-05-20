92e26369ec0a31001d982206837c57ab2f59ff3a
xdcrazyboy <fengziboboy@163.com>
2020-05-21 02:52:33 +0800

    update 521

---
+++ SpringInAction5/Class1/src/main/java/com/fengziboboy/demo/web/DesignTacoController.java
@@ -0,0 +1,10 @@
+package com.fengziboboy.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+}
--- SpringInAction5/Class1/src/main/java/com/fengziboboy/demo/web/HomeController.java
+++ SpringInAction5/Class1/src/main/java/com/fengziboboy/demo/web/HomeController.java
@@ -1,4 +1,4 @@
-package com.fengziboboy.demo;
+package com.fengziboboy.demo.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
